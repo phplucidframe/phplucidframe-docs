67bef2697136ffea45c05776b7e4736fdd62c0c0
Sithu <cithukyaw@gmail.com>
2024-10-13 20:46:21 +0630

    Tell Jinja2 templates the build is running on Read the Docs

--- conf.py
+++ conf.py
@@ -21,6 +21,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
 
 # -- General configuration ------------------------------------------------
 
@@ -157,3 +158,12 @@ texinfo_documents = [
      author, 'PHPLucidFrame', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+# html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
